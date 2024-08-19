6ebbfe1909acd2367993df70e88032f7065f736f
Bivek Karki <BivekKarki>
2024-08-19 22:37:11 +0545

    feat: add button use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
+import ProductCard from "./components/productcard";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
       <Link href="/users" >users</Link>
+      <Link href={"/users/new"}>New users</Link>
+      <ProductCard />
     </main>
   );
 }
